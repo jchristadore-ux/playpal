19b24148297bfd044f7e90a6890a1c503a633580
jchristadore-ux <jchristadore@gmail.com>
2026-04-26 10:34:00 -0400

    Fix lone parameter logic in holeResult calculation

--- components/Trackers.jsx
+++ components/Trackers.jsx
@@ -44,7 +44,7 @@ const WolfTracker = ({ players, scores, wolfData, course, holeIdx, onSetPartner,
   // Hole result if confirmed
   const holeResult = React.useMemo(() => {
     if (!wd.confirmed) return null;
-    return resolveWolfHole(scores, holeIdx, wd.wolfId, wd.partnerId, !!wd.lone, players, course.holes[holeIdx]);
+    return resolveWolfHole(scores, holeIdx, wd.wolfId, wd.partnerId, !wd.lone, players);
   }, [wd.confirmed, JSON.stringify(scores), holeIdx]);
 
   const maxPts    = Math.max(...players.map(p => standings[p.id]||0));
